0556a78570ece6ef18ee168cbd7eaa95bce83deb
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2021-02-04 21:35:28 -0800

    Setup for Hmescrren without a card

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Product } from "../products";
+const HomeScreen = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default HomeScreen
