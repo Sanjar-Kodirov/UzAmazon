976c556a442c7834e0c914a3cf4434f602b8e543
Sanjar-Kodirov <57550159+Sanjar-Kodirov@users.noreply.github.com>
2021-02-19 22:39:33 -0800

    First Commit on desktop version
    there is no changes

--- frontend/src/component/Product.js
+++ frontend/src/component/Product.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Row,Col } from 'react-bootstrap'
 
 const Product = ({product}) => {
     return (
         <div>
             <Card className="my-3 p-3 rounded-4">
-                
                     <a href={`/product/${product._id}`}>
                         <Card.Img src={product.image} varient='right' />
                     </a>
                 <Card.Body>
-                    
+                    <Card.Title as='div' varient='right'>
+                        <div className='my-3'>
+                           <h1> {product.name} </h1>
+                        </div>
+                    </Card.Title>
+                    <Card.Title as='div' varient='right'>
+                        <div className='my-3'>
+                            {product.description} from {product.numReviews} reviews 
+                        </div>
+                    </Card.Title>
                     <Card.Title as='div' varient='right'>
                         <div className='my-3'>
                             {product.rating} from {product.numReviews} reviews 
